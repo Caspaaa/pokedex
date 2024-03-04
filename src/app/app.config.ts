ed0ee909ee78287020ba315c5720d3bfdc33d494
Eloi-Semetis <eloi@semetis.com>
2024-03-04 16:33:56 +0100

    Add Apollo & update pokeapi service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { graphqlProvider } from './graphql.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideHttpClient(), graphqlProvider,
   ],
 };
