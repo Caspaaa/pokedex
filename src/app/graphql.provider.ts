35c322054555f0a916a9eb5761d179c7dab224fb
Eloi-Semetis <eloi@semetis.com>
2024-03-05 09:40:41 +0100

    Update all form REST to GraphQL

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -1,14 +1,35 @@
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import {
+  ApolloClientOptions,
+  ApolloLink,
+  InMemoryCache,
+} from '@apollo/client/core';
+import { onError } from '@apollo/client/link/error';
 
 const uri = 'https://beta.pokeapi.co/graphql/v1beta';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
-    link: httpLink.create({ uri }),
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            )
+          );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+      }),
+      httpLink.create({ uri }),
+    ]),
     cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-first',
+      },
+    },
   };
 }
 
--- src/app/pokeapi.service.ts
+++ src/app/pokeapi.service.ts
@@ -4,19 +4,30 @@ import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
 import { Apollo, gql } from 'apollo-angular';
 
-interface PokemonListResponse {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Array<{
-    name: string;
-    url: string;
+interface Pokemon {
+  base_experience: number;
+  height: number;
+  id: number;
+  name: string;
+  order: number;
+  is_default: boolean;
+  pokemon_v2_pokemonsprites: Array<{
+    id: number;
+    sprites: {
+      front_default: string;
+    };
   }>;
 }
 
+interface PokemonListResponse {
+  data: {
+    pokemon_v2_pokemon: Pokemon[];
+  };
+}
+
 const GET_POKEMONS = gql`
-  query getPokemons {
-    pokemon_v2_pokemon(offset: 0, limit: 20) {
+  query getPokemons($offset: Int!, $limit: Int!) {
+    pokemon_v2_pokemon(offset: $offset, limit: $limit) {
       base_experience
       height
       id
@@ -35,65 +46,28 @@ const GET_POKEMONS = gql`
   providedIn: 'root',
 })
 export class PokeapiService {
+  private limit = 20;
+  private offset = 0;
+
   constructor(private apollo: Apollo) {}
 
-  fetchPokemons() {
+  fetchPokemons(): Observable<PokemonListResponse> {
     return this.apollo.watchQuery<any>({
       query: GET_POKEMONS,
+      variables: {
+        offset: this.offset,
+        limit: this.limit,
+      },
     }).valueChanges;
   }
+
+  nextPokemons() {
+    this.offset += this.limit;
+    return this.fetchPokemons();
+  }
+
+  previousPokemons() {
+    this.offset = Math.max(0, (this.offset -= this.limit));
+    return this.fetchPokemons();
+  }
 }
-// export class PokeapiService {
-//   private allPokemonsCache: Array<{ name: string; url: string }> | null = null;
-
-//   constructor(
-//     private http: HttpClient,
-//     private localStorageService: LocalStorageService
-//   ) {}
-
-//   fetchPokemons(
-//     url: string = 'https://pokeapi.co/api/v2/pokemon?limit=20'
-//   ): Observable<PokemonListResponse> {
-//     const cachedData: PokemonListResponse =
-//       this.localStorageService.getItem(url);
-//     if (cachedData !== null) {
-//       // of() function is use because we need to return an Observable in both cases
-//       return of(cachedData);
-//     } else {
-//       return this.http.get<PokemonListResponse>(url).pipe(
-//         // why do we need to use pipe()
-//         tap((data) => this.localStorageService.setItem(url, data)),
-//         catchError(
-//           (error: HttpErrorResponse): Observable<PokemonListResponse> => {
-//             console.error('Error fetching Pokémon list', error);
-//             return throwError(
-//               () => new Error('An error occurred while fetching the pokemons')
-//             );
-//           }
-//         )
-//       );
-//     }
-//   }
-
-//   fetchAllPokemonse(): Observable<Array<{ name: string; url: string }>> {
-//     const cachedPokemons = this.localStorageService.getItem('allPokemons');
-//     if (cachedPokemons) {
-//       this.allPokemonsCache = cachedPokemons;
-//       return of(cachedPokemons);
-//     } else {
-//       return this.http
-//         .get<any>('https://pokeapi.co/api/v2/pokemon?limit=100000')
-//         .pipe(
-//           map((response) => response.results),
-//           tap((pokemons) => {
-//             this.allPokemonsCache = pokemons;
-//             this.localStorageService.setItem('allPokemons', pokemons);
-//           })
-//         );
-//     }
-//   }
-
-//   getAllPokemonsCache(): Array<{ name: string; url: string }> | null {
-//     return this.allPokemonsCache;
-//   }
-// }
