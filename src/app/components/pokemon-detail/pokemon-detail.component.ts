c242c5342d910437bceb2a894ebe9255eb1c8957
Eloi-Semetis <eloi@semetis.com>
2024-03-08 11:43:15 +0100

    Add navigation on Pokemon detail page

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,37 +1,48 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import {
+  ActivatedRoute,
+  RouterOutlet,
+  RouterLink,
+  Router,
+} from '@angular/router';
+
 import { PokeapiService } from '../../services/pokeapi.service';
-import { PokemonDetails } from '@models/pokemon.model';
+import { PokemonFull } from '@models/pokemon.model';
 import { NgIf } from '@angular/common';
+import { PokemonSearchComponent } from '../pokemon-input/pokemon-input.component';
+import { map, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, RouterLink, NgIf, PokemonSearchComponent],
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.css',
 })
 export class PokemonDetailComponent {
-  pokemonDetails: PokemonDetails | null = null;
+  PokemonFull: PokemonFull | null = null;
+
   constructor(
     private pokeapiService: PokeapiService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    this.loadPokemonDetails();
+    const UriPokemonId = this.route.params.pipe(map((params) => params['id']));
+
+    UriPokemonId.subscribe((id) => {
+      this.loadPokemonFull(id);
+    });
   }
 
-  loadPokemonDetails(): void {
-    const pokemonId: number = Number(this.route.snapshot.paramMap.get('id'));
-    if (pokemonId) {
-      this.pokeapiService
-        .fetchPokemonDetails(pokemonId)
-        .subscribe((response) => {
-          if (response) {
-            this.pokemonDetails = response.data.pokemon_v2_pokemon[0];
-          }
-        });
-    }
+  loadPokemonFull(id: number) {
+    this.pokeapiService
+      .fetchPokemonFull(id)
+      .subscribe((PokemonFull: PokemonFull) => {
+        if (PokemonFull) {
+          this.PokemonFull = PokemonFull;
+        }
+      });
   }
 }
