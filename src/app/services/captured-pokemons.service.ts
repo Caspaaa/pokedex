566b4c80679646ef7d2b914c1e8522380deb6810
Eloi Baudelle <e.baudelle@gmail.com>
2024-03-11 10:09:05 +0100

    Update captured-pokemon-service to use pokemon-data service

--- src/app/services/captured-pokemons.service.ts
+++ src/app/services/captured-pokemons.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@angular/core';
-import { LocalStorageService } from './local-storage.service';
+import { PokemonDataService } from './pokemon-data.service';
 import { PokemonLight } from '@models/pokemon.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CapturedPokemonsService {
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private pokemonDataService: PokemonDataService) {}
 
   toggleCapturedStatus(id: number): void {
-    let { expiringTime, allPokemons } = JSON.parse(
-      this.localStorageService.getItem('list')
-    );
-    const pokemonIndex = allPokemons.findIndex(
-      (pokemon: PokemonLight) => pokemon.id === id
-    );
-    if (pokemonIndex !== -1) {
-      allPokemons[pokemonIndex].captured = !allPokemons[pokemonIndex].captured;
-      this.localStorageService.setItem(
-        'list',
-        JSON.stringify({ expiringTime, allPokemons })
-      );
+    const currentList = this.pokemonDataService.pokemonList;
+    if (currentList) {
+      const currentPokemon = currentList.find((pokemon) => pokemon.id);
+      if (currentPokemon) currentPokemon.captured = !currentPokemon.captured;
+      this.pokemonDataService.updateLocalStorage(currentList);
     }
   }
 }
