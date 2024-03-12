fb1e14ee2064a40423680dad6b1af7e42ea383a8
Eloi-Semetis <eloi@semetis.com>
2024-03-12 16:47:13 +0100

    Add home button to menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { PokemonSearchComponent } from '../pokemon-input/pokemon-input.component';
 import { CommonModule, NgFor, NgStyle } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
-  imports: [PokemonSearchComponent, NgStyle],
+  imports: [PokemonSearchComponent, RouterLink, NgStyle],
 })
 export class MenuComponent {
   @Output() togglePanel = new EventEmitter<void>();
