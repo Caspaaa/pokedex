64d14b65e3bc0734a6ccc8c87d382ffbd4962a6a
Eloi Baudelle <e.baudelle@gmail.com>
2024-03-12 18:43:45 +0100

    Add error component & adapt routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 import { PokemonsListComponent } from './components/pokemons-list/pokemons-list.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
   { path: '', component: PokemonsListComponent },
   { path: 'pokemon/:id', component: PokemonDetailComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/error' },
 ];
