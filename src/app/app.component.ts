e1636e2a0e4fde3b315ab0b343e466f47fa2be5f
Eloi Baudelle <e.baudelle@gmail.com>
2024-03-12 09:30:48 +0100

    Create menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, SidePanelComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    SidePanelComponent,
+    MenuComponent,
+  ],
 })
 export class AppComponent {
   title = 'pokedex';
