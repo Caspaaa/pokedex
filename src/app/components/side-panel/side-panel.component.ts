7dfe71c0833503a5b2b60ec9ca31b5c80a56e593
Eloi Baudelle <e.baudelle@gmail.com>
2024-03-14 09:34:03 +0100

    Update side-panel

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -3,20 +3,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Pokemon } from '@models/pokemon.model';
 import { tap } from 'rxjs';
-import { ClickOutsideDirective } from '../../directives/click-outside.directive';
 import { PokemonDataService } from '../../services/pokemon-data.service';
 import { MyPokemonsComponent } from '../my-pokemons/my-pokemons.component';
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [
-    MyPokemonsComponent,
-    NgFor,
-    NgIf,
-    RouterLink,
-    ClickOutsideDirective,
-  ],
+  imports: [MyPokemonsComponent, NgFor, NgIf, RouterLink],
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.css'],
 })
